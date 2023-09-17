a9f79ffc318ab37a42a786886a6277ce2bae91ec
Rohman-farisi <rohmanfarisi01@gmail.com>
2023-09-17 22:43:25 +0700

    Input perbaikan tugas module system

---
+++ module1.js
@@ -0,0 +1,13 @@
+import {Table} from "./module2"
+
+const table = new Table({
+    columns: ["NO", "Nama", "No. HP"],
+    data: [
+        [1, "Rohman Farisi", '085711050652'],
+        [2, "Agus Salim", '087771288344'],
+        [3, "Silvia Tita", '0877752417724']
+    ]
+});
+
+const app = document.getElementById("app");
+  table.render(app);
